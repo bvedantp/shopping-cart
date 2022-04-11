226af0b33a32aa33a1702f0f5ea1a1d389764191
B Vedant Pandey <bvedantp@gmail.com>
2022-04-11 15:51:31 +0530

    styled homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,27 +1,31 @@
 import React from "react";
 import Nav from "./Nav";
 import GlobalStyles from "./styles/Global";
+import { faPersonWalkingArrowRight } from "@fortawesome/free-solid-svg-icons";
+import { HomeBase, Heading, CanvasArea, 
+    TextArea, WelcomeText, IconBorder } from "./styles/Home.styled";
 
 export default function Home() {
     return(
         <>
             <GlobalStyles />
             <Nav />
-            <div>
-                <h1>Glowing(glowing glows) Crystals(this does not)</h1>
-                <div>
-                    <p>Placeholder for 3d model</p>
-                </div>
-                <div>
-                    <ul>
-                        <li>Ho Ho Ho</li>
-                        <li>Ho Ho Ho</li>
-                        <li>Hoe Ho Hoe</li>
-                        <li>Ho Ho Ho</li>
-                    </ul>
-                    <button>Shop Now</button>
-                </div>
-            </div>
+            <HomeBase>
+                <Heading>Glow Show</Heading>
+                <CanvasArea>
+                    <canvas></canvas>
+                </CanvasArea>
+                <TextArea>
+                    <WelcomeText>
+                        <p>Our features:</p>
+                        <li>Shop around to find <b>glowing</b> rocks that give various abilities.</li>
+                        <li>Found from the rarest of places, we provide only the <b>top notch</b> crystals.</li>
+                        <li>Home delivery available at no extra cost.</li>
+                        <li>Get <b>exclusive</b> deals as you level up.</li>
+                    </WelcomeText>
+                    <button>Shop Now <IconBorder icon={faPersonWalkingArrowRight} /> </button>
+                </TextArea>
+            </HomeBase>
         </>
     )
 }
--- src/components/styles/Home.styled.js
+++ src/components/styles/Home.styled.js
@@ -1,2 +1,82 @@
-import styledComponents from "styled-components";
+import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
+export const HomeBase = styled.div`
+    margin: 3vh 12px;
+    padding: 10px;
+    display: flex;
+    gap: 2em;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`
+
+export const Heading = styled.h1`
+    color: #FFD42F;
+    margin: 0;
+    padding: 5px;
+    font-size: 3em;
+`
+
+export const CanvasArea = styled.div`
+    position: fixed;
+    top: 20%;
+    left: -10%;
+    width: 50vw;
+    height: 70vh;
+    
+`
+
+export const TextArea = styled.section`
+    align-self: flex-end;
+    display: flex;
+    flex-direction: column;
+    gap: 1.5em;
+    width: 50vw;
+    
+    /* position: fixed;
+    padding: 2rem;
+    right: -4%;
+    top: 30vh; */
+    button {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 4px 12px;
+        border: 2px solid #FFD42F;
+        border-radius: 6px;
+        color: #FFD42F;
+        background-color: rgba(255, 213, 47, 0);
+        text-align: right;
+
+        &:hover {
+            background-color: #FFD42F;
+            color: #1B2025;
+        }
+    }
+
+`
+
+export const WelcomeText = styled.ul`
+    line-height: 1.2em;
+    
+    p {
+        font-weight: 700;
+        margin-bottom: 15px;
+    }
+
+    li::marker {
+        color: #FFD42F;
+        font-weight: bold;
+    }
+
+    b{
+        color: #FFD42F;
+    }
+`
+
+export const IconBorder = styled(FontAwesomeIcon)`
+    border-left: 3px solid #FFD42F;
+    padding-left: 9px;
+
+`
